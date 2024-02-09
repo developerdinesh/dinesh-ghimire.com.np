e0883f1df47eb4fa6bacf681303d34fc75c6cf6e
Dinesh Ghimire <developer.dinesh1@gmail.com>
2024-02-09 21:34:21 +0545

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 // src/App.tsx
 
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes/Routes';
 import Header from './components/common/Header/Header';
 import Footer from './components/common/Footer/Footer';
 
 const App: React.FC = () => {
   return (
-    <Router>
       <div className="App">
-        <h1>Check</h1>
         <Header />
         <main>
           <Routes />
         </main>
         <Footer />
       </div>
-    </Router>
   );
 };
 

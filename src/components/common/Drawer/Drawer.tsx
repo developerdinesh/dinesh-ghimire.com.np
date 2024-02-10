39cf1b79466f46aac24023c02d5fb074dda7fefe
Dinesh Ghimire <developer.dinesh1@gmail.com>
2024-02-10 17:47:46 +0545

    changes

--- src/components/common/Drawer/Drawer.tsx
+++ src/components/common/Drawer/Drawer.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Drawer: React.FC = () => {
+const Drawer = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <p>This is Drawer Content.</p>
+    <div className={`drawer ${isOpen ? 'open' : ''}`}>
+      <button className="toggle-drawer-button" onClick={toggleDrawer}>
+        {isOpen ? 'Close Drawer' : 'Open Drawer'}
+      </button>
+      <div className="drawer-content">
+        <ul className="menu-items">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          {/* Add more menu items as needed */}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Drawer from '../../components/common/Drawer/Drawer';
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className='page-home'>
+      <Drawer/>
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page content.</p>
     </div>
